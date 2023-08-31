177a344a97f60da5540d30c89fbc4cfd32f86f23
Kevin Adler <kadler@us.ibm.com>
2023-08-31 12:27:21 -0500

    Raise error if passed a TZ-aware datetime
    This is similar to MySQL and Oracle backends.

--- django_ibmi/operations.py
+++ django_ibmi/operations.py
@@ -291,6 +291,22 @@ class DatabaseOperations (BaseDatabaseOperations):
             return "UPPER(%s)"
         return "%s"
 
+    def adapt_datetimefield_value(self, value):
+        if value is None:
+            return None
+
+        # Expression values are adapted by the database.
+        if hasattr(value, 'resolve_expression'):
+            return value
+
+        # We don't support tz-aware datetimes
+        if timezone.is_aware(value):
+            if settings.USE_TZ:
+                value = timezone.make_naive(value, self.connection.timezone)
+            else:
+                raise ValueError("Db2 fori backend does not support timezone-aware datetimes when USE_TZ is False.")
+
+        return str(value)
 
     def max_name_length(self):
         return 128
